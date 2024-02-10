ab647f25b4f169e8309e0721de639f634cd33cd4
Key <agogirl07@gmail.com>
2024-02-09 19:27:52 -0500

    fixed customerlist

--- ghi/app/src/CustomerList.js
+++ ghi/app/src/CustomerList.js
@@ -1,13 +1,13 @@
 import  { useState, useEffect} from 'react';
 
 function CustomerList() {
-    const [customers, setCustomer] = useState ([])
+    const [customer, setCustomer] = useState ([])
 
     const getData = async () => {
         const response = await fetch ('http://localhost:8090/api/customers/')
         if (response.ok) {
-            const { customers } = await response.json();
-            setCustomer(customers);
+            const { customer } = await response.json();
+            setCustomer(customer);
         }else {
             console.error('An error occured fetching the data')
         }
@@ -33,7 +33,7 @@ useEffect(()=> {
             </tr>
           </thead>
           <tbody>
-            {customers.map(tech => {
+            {customer.map(tech => {
               return (
                 <tr key={tech.id}>
                   <td>{ tech.first_name }</td>
